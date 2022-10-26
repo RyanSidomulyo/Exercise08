8616109e1250b487916e56471850e74208497897
ryan <sidomulyo784@gmail.com>
2022-10-26 17:03:28 +0800

    menambahkan exercise 03

---
+++ Documents/FrontEndDev/tgs3/index.js
@@ -0,0 +1,56 @@
+console.log("Assignment3");
+let person = [
+    {
+      name: "John",
+      age: 35,
+    },
+    {
+      name: "Bill",
+      age: 23,
+    },
+    {
+      name: "Bob",
+      age: 17,
+    },
+    {
+      name: "Jack",
+      age: 40,
+    },
+  ];
+let max = person[0].age;
+
+for (let i = 1; i < person.length; ++i) {
+
+  if (person[i].age > max) {
+
+    max = person[i].age;
+
+  }
+
+}
+console.log("umur Tertua adalah : " + max);
+
+let min = person[0].age;
+
+for (let i = 1; i < person.length; ++i) {
+
+  if (person[i].age < min) {
+
+    min = person[i].age;
+
+  }
+
+}
+console.log("Umur Termuda adalah : " + min);
+index.js
+Displaying index.js. 
+Exercise #03
+Stenly Adam
+•
+Sep 5
+100 points
+Due Sep 5, 9:00 PM
+
+index.js
+Javascript
+Class comments
